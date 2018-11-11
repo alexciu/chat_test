d106ec373491233d5dfd3afb424a3e64624511a4
Alex Ciu <alexcm8686@yahoo.com>
2018-11-11 20:16:17 +0200

    2

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
-
 let app = express();
-
+let server = http.createServer(app)
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log("User conected")
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected.')
+    })
+})
 //ddd
 
 
-app.listen(port , () => {
+server.listen(port , () => {
     console.log(`Server start on port ${port}.`)
 }); 
