5acecd39113d8d3a0d1b954d6ed43f6a1368a7d2
Alex Ciu <alexcm8686@yahoo.com>
2018-11-18 19:53:19 +0200

    update user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -20,11 +20,30 @@ function scrollToBottom(){
 
 socket.on('connect', function () {
   console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search)
+
+  socket.emit('join', params, function(err){
+    if(err){
+      alert(err);
+      window.location.href = '/';
+    } else {
+
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+
+socket.on('updateUserList', function(users){
+  console.log(users);
+  var ol = $('<ol></ol>');
+  users.forEach(function (user) {
+    ol.append($('<li></li>').text(user))
+  });
+  $('#users').html(ol)
+})
 
 socket.on('newMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('h:mm a')
